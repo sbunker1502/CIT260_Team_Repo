1369985c3d51d3c8ff9f1b66ae43aa212cdd578c
seanbunker <seanbunker@172.20.10.132>
2018-10-11 11:58:38 -0700

    10/11/18 Created Project 10/11/18 - 11:57AM MST Comments Modified

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Winter 2018
+ * Team members: Shawn Curtis, Tyler Watson, Sean Bunker
  */
 package cityofaaron;
 
