c2b0b415f6df4dcf9c04f42b10169a031b2b3e1d
Shawn <shawn.d.curtis@gmail.com>
2018-10-11 19:00:19 -0600

    10/11/18 team assignment

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -12,11 +12,13 @@ import model.*;
  */
 public class CityOfAaron {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+   public static void main(String[] args) {
+      Player player1 = new Player();
+      player1.setName("Shawn");
+      
+      Game game1 = new Game();
+      game1.setPlayer(player1);
+      System.out.println(game1.getPlayer().getName() + "\n");
+      System.out.println(TeamMembers.withAnH.getTitle() + " " + TeamMembers.withAnH.getName() + "\n" + TeamMembers.withAnE.getTitle() + " " + TeamMembers.withAnE.getName() + "\n" + TeamMembers.withAT.getTitle() + " " + TeamMembers.withAT.getName());
+   }    
 }
