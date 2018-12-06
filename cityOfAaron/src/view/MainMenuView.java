95ce693b1c39422e34b34bd64ce0be5517157415
seanbunker <seanbunker@192.168.0.113>
2018-12-05 19:19:00 -0700

    Week 12 Changes - SB with getSavedGame()

--- cityOfAaron/src/view/MainMenuView.java
+++ cityOfAaron/src/view/MainMenuView.java
@@ -4,98 +4,117 @@
  * and open the template in the editor.
  */
 package view;
+
 import model.*;
 import control.*;
 import cityofaaron.CityOfAaron;
 
 import java.util.Scanner;
+
 /**
  *
  * @author Team Awesome
  */
 
-public class MainMenuView extends MenuView{
-   // The MainMenuView constructor
-   // Purpose: Initialize the menu data
-   // Parameters: none
-   // Returns: none
-   // ===================================
-   public MainMenuView( ){
-      super("\n" +
-                 "**********************************\n" +
-                 "* CITY OF AARON: MAIN GAME MENU  *\n" +
-                 "**********************************\n" +
-                 " 1 - Start new game\n" +
-                 " 2 - Get and start a saved game\n" +
-                 " 3 - Get help on playing the game\n" +
-                 " 4 - Save game\n" +
-                 " 5 - Quit\n", 
-            5);
-   }
+public class MainMenuView extends MenuView {
+    // The MainMenuView constructor
+    // Purpose: Initialize the menu data
+    // Parameters: none
+    // Returns: none
+    // ===================================
 
+    public MainMenuView() {
+        super("\n"
+                + "**********************************\n"
+                + "* CITY OF AARON: MAIN GAME MENU  *\n"
+                + "**********************************\n"
+                + " 1 - Start new game\n"
+                + " 2 - Get and start a saved game\n"
+                + " 3 - Get help on playing the game\n"
+                + " 4 - Save game\n"
+                + " 5 - Quit\n",
+                5);
+    }
 
-   // The doAction method
-   // Purpose: performs the selected action
-   // Parameters: none
-   // Returns: none
-   // ===================================       
-   @Override public void doAction(int option)
-   {
-      switch(option){
-         case 1: // create and start a new game
-             startNewGame();
-             break;
-         case 2: // get and start a saved game
-             startSavedGame();
-             break;
-         case 3: // get help menu
-             displayHelpMenuView();
-             break;
-         case 4: // save game
-             displaySaveGameView();
-             break;
-         case 5:
-             System.out.println("Thanks for playing ... goodbye.");
-      }
-      return;
-   } 
+    // The doAction method
+    // Purpose: performs the selected action
+    // Parameters: none
+    // Returns: none
+    // ===================================       
+    @Override
+    public void doAction(int option) {
+        switch (option) {
+            case 1: // create and start a new game
+                startNewGame();
+                break;
+            case 2: // get and start a saved game
+                startSavedGame();
+                break;
+            case 3: // get help menu
+                displayHelpMenuView();
+                break;
+            case 4: // save game
+                displaySaveGameView();
+                break;
+            case 5:
+                System.out.println("Thanks for playing ... goodbye.");
+        }
+        return;
+    }
 
-   public void startNewGame(){
-      // Display the Banner Page.
-      System.out.println("\nWelcome to the city of Aaron.");
+    public void startNewGame() {
+        // Display the Banner Page.
+        System.out.println("\nWelcome to the city of Aaron.");
 
-       // Prompt for and get the user’s name.
-      String name;
-      System.out.println("\nPlease type in your first name: ");
-      name = keyboard.next();
+        // Prompt for and get the user’s name.
+        String name;
+        System.out.println("\nPlease type in your first name: ");
+        name = keyboard.next();
 
-      // Call the createNewGame() method in GameControl class
-      // pass the name as a parameter
-      GameControl.createNewGame(name);
+        // Call the createNewGame() method in GameControl class
+        // pass the name as a parameter
+        GameControl.createNewGame(name);
 
-      // Display a welcome message
-      System.out.println("Welcome " + name + " have fun!!!");
+        // Display a welcome message
+        System.out.println("Welcome " + name + " have fun!!!");
 
-      // Display the Game menu
-      GameMenuView gmv = new GameMenuView();
-      gmv.displayMenu();
+        // Display the Game menu
+        GameMenuView gmv = new GameMenuView();
+        gmv.displayMenu();
 
-   }
+    }
 
-   
-   public void startSavedGame(){
-       System.out.println("\n Start saved game option selected.");
-   }
-   
-           
-   public void displayHelpMenuView(){
-      System.out.println("\n Display help menu selected.");
-      HelpMenuView hmv = new HelpMenuView();
-      hmv.displayMenu();
-   }
-   
-           
-   public void displaySaveGameView(){
-       System.out.println("\n Display save game selected.");
-   }
+    // The startSavedGame method
+    // Purpose: loads a saved game object from disk and start the game
+    // Parameters: none
+    // Returns: none
+    // ===================================    
+    public void startSavedGame() {
+        System.out.println("\n Start saved game option selected.");
+        
+        Scanner input = new Scanner(System.in);
+
+        // get rid of nl character left in the stream
+        input.nextLine();
+        // prompt user and get a file path
+        System.out.println("File path of the game wanted: ");
+        String fileName;
+        fileName = input.nextLine();
+        // call the getSavedGame( ) method in the GameControl class to load the game
+        GameControl gc = new GameControl();
+        gc.getSavedGame(fileName);        
+        // display the game menu for the loaded game
+        GameMenuView gm = new GameMenuView();
+        gm.displayMenu();
+    }
+
+    public void displayHelpMenuView() {
+        System.out.println("\n Display help menu selected.");
+        HelpMenuView hmv = new HelpMenuView();
+        hmv.displayMenu();
+    }
+
+    public void displaySaveGameView() {
+        System.out.println("\n Display save game selected.");
+    }
 }
