41b9cf77d637922f3626efa7ee4bf5c9c5e09063
thomaswatson <thomaswatson@192.168.0.33>
2018-12-06 19:44:06 -0600

    11/20/18 - Worked save animal list in the game control

--- cityOfAaron/src/control/GameControl.java
+++ cityOfAaron/src/control/GameControl.java
@@ -193,8 +193,33 @@ public class GameControl {
 
        //save the animals in the game
       // Game.setAnimals(animals);
-   }
-        
+   }  
+        //method to save the animals list to disk 
+        public static void saveAnimalList() {
+            Scanner keyboard = new Scanner(System.in);
+            //receive a string of the file name, passed into the printing routine.
+            String listPath;
+            System.out.println("Please enter a file path for the animals list: ");
+            listPath = keyboard.next();
+            
+            
+            //declare a reference to a PrintWriter object
+            try (PrintWriter out = new PrintWriter(listPath);) {
+                //create the PrintWriter object
+                
+                //get a reference to the ArrayList
+                ArrayList<ListItem> animals = Game.getAnimals();
+                
+                //output a heading for the report
+                out.println("\n\n Animals List      "); 
+                //use a for loop to get the data from the ArrayList
+                for (ListItem item : animals) {
+                out.printf("%n%-20s%7d", item.getName()
+                                             , item.getNumber());
+            
+                }
+}
+    /**    
    // method to display animals list
    public static void showAnimalsList() {
        System.out.println("showAnimalsList Method Called");
